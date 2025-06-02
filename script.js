25f52753d623cc1453d22eecd8ac7efcf1f552b6
kelvin mandrakudo do tg <141235137+silvakelvin17@users.noreply.github.com>
2025-06-02 09:07:24 -0300

    ponto

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const caixaPrincipal = document.querySelector("caixa-principal");
-const caixaPerguntas = document.querySelector("caixa-perguntas");
-const caixaAlternativas = document.querySelector("caixa-alternativas");
-const caixaResultado = document.querySelector("caixa-resultado");
-const textoResultado = document.querySelector("texto-resultado");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
         enunciado: "Pergunta 1",
